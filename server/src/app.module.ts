ccf9967ef021f18206cce69dd98d184eb1177796
Esrael27 <biruktechnologies@gmail.com>
2023-12-03 08:01:29 +0300

    test

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,14 +7,13 @@ import { ProjectsService } from './projects/projects.service';
 import { ProjectsController } from './projects/projects.controller';
 import { TaskController } from './tasks/tasks.controller';
 import { TaskService } from './tasks/tasks.service';
-import { IssueController } from './issues/issues.controller';
-import { IssueService } from './issues/issues.service';
+
 
 
 
 @Module({
-  controllers: [UserController,ProjectsController,TaskController, IssueController ],
-  providers: [UserService,ProjectsService,TaskService,IssueService,PrismaService],
+  controllers: [UserController,ProjectsController,TaskController,],
+  providers: [UserService,ProjectsService,TaskService,PrismaService],
 })
 export class AppModule {}
 
