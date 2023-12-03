f692c3e87b95b075ed454be10423de12a7b53cfe
Esrael27 <biruktechnologies@gmail.com>
2023-12-03 07:52:10 +0300

    a

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './users/users.controller';
+import { UserService } from './users/users.service';
+
+import { PrismaService } from './prisma/prisma.service';
+import { ProjectsService } from './projects/projects.service';
+import { ProjectsController } from './projects/projects.controller';
+import { TaskController } from './tasks/tasks.controller';
+import { TaskService } from './tasks/tasks.service';
+import { IssueController } from './issues/issues.controller';
+import { IssueService } from './issues/issues.service';
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UserController,ProjectsController,TaskController, IssueController ],
+  providers: [UserService,ProjectsService,TaskService,IssueService,PrismaService],
 })
 export class AppModule {}
+
--- server/src/issues/issues.service.ts
+++ server/src/issues/issues.service.ts
@@ -1,107 +1,107 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { CreateIssueDto , UpdateIssueDto  } from './dto/issue.dto';
+// import { Injectable, NotFoundException } from '@nestjs/common';
+// import { PrismaService } from '../prisma/prisma.service';
+// import { CreateIssueDto , UpdateIssueDto  } from './dto/issue.dto';
 
 
-@Injectable()
-export class IssueService {
-  constructor(private readonly prisma: PrismaService) {}
+// @Injectable()
+// export class IssueService {
+//   constructor(private readonly prisma: PrismaService) {}
 
-  // Create a new Issue
-  // Inside IssueService
-async createIssue(createIssueDto: CreateIssueDto) {
-  try {
-    const issue = await this.prisma.issue.create({
-      data: createIssueDto,
-    });
+//   // Create a new Issue
+//   // Inside IssueService
+// async createIssue(createIssueDto: CreateIssueDto) {
+//   try {
+//     const issue = await this.prisma.issue.create({
+//       data: createIssueDto,
+//     });
 
-    return issue;
-  } catch (error) {
-    throw new Error(`Failed to create issue: ${error.message}`);
-  }
-}
+//     return issue;
+//   } catch (error) {
+//     throw new Error(`Failed to create issue: ${error.message}`);
+//   }
+// }
 
 
-  // Get all issue
-  async getAllIssues() {
-    try {
-      return await this.prisma.issue.findMany();
-    } catch (error) {
-      throw new Error(`Failed to get issue: ${error.message}`);
-    }
-  }
+//   // Get all issue
+//   async getAllIssues() {
+//     try {
+//       return await this.prisma.issue.findMany();
+//     } catch (error) {
+//       throw new Error(`Failed to get issue: ${error.message}`);
+//     }
+//   }
 
-  // Get a issue by ID
-  async getIssuesById(id: number) {
-    try {
-      const issue = await this.prisma.issue.findUnique({
-        where: {
-          id,
-        },
-      });
+//   // Get a issue by ID
+//   async getIssuesById(id: number) {
+//     try {
+//       const issue = await this.prisma.issue.findUnique({
+//         where: {
+//           id,
+//         },
+//       });
 
-      if (!issue) {
-        throw new NotFoundException('issue not found');
-      }
+//       if (!issue) {
+//         throw new NotFoundException('issue not found');
+//       }
 
-      return issue;
-    } catch (error) {
-      throw new Error(`Failed to get issue by ID: ${error.message}`);
-    }
-  }
+//       return issue;
+//     } catch (error) {
+//       throw new Error(`Failed to get issue by ID: ${error.message}`);
+//     }
+//   }
 
-  // Update a issue by ID
-  async updateIssue(id: number,UpdateIssueDto  : UpdateIssueDto  ) {
-    try {
-      // Check if the issue exists
-      const existingissue = await this.prisma.issue.findUnique({
-        where: {
-          id,
-        },
-      });
+//   // Update a issue by ID
+//   async updateIssue(id: number,UpdateIssueDto  : UpdateIssueDto  ) {
+//     try {
+//       // Check if the issue exists
+//       const existingissue = await this.prisma.issue.findUnique({
+//         where: {
+//           id,
+//         },
+//       });
 
-      if (!existingissue) {
-        throw new NotFoundException('issue not found');
-      }
+//       if (!existingissue) {
+//         throw new NotFoundException('issue not found');
+//       }
 
-      // Update the issue
-      const updatedissue= await this.prisma.issue.update({
-        where: {
-          id,
-        },
-        data: UpdateIssueDto  ,
-      });
+//       // Update the issue
+//       const updatedissue= await this.prisma.issue.update({
+//         where: {
+//           id,
+//         },
+//         data: UpdateIssueDto  ,
+//       });
 
-      return updatedissue;
-    } catch (error) {
-      throw new Error(`Failed to update issue: ${error.message}`);
-    }
-  }
+//       return updatedissue;
+//     } catch (error) {
+//       throw new Error(`Failed to update issue: ${error.message}`);
+//     }
+//   }
 
-  // Delete a issue by ID
-  async deleteIssue(id: number) {
-    try {
-      // Check if the issue exists
-      const existingissue = await this.prisma.issue.findUnique({
-        where: {
-          id,
-        },
-      });
+//   // Delete a issue by ID
+//   async deleteIssue(id: number) {
+//     try {
+//       // Check if the issue exists
+//       const existingissue = await this.prisma.issue.findUnique({
+//         where: {
+//           id,
+//         },
+//       });
 
-      if (!existingissue) {
-        throw new NotFoundException('issue not found');
-      }
+//       if (!existingissue) {
+//         throw new NotFoundException('issue not found');
+//       }
 
-      // Delete the issue
-      await this.prisma.issue.delete({
-        where: {
-          id,
-        },
-      });
+//       // Delete the issue
+//       await this.prisma.issue.delete({
+//         where: {
+//           id,
+//         },
+//       });
 
-      return { success: true, message: 'issue deleted successfully' };
-    } catch (error) {
-      throw new Error(`Failed to delete issues: ${error.message}`);
-    }
-  }
-}
+//       return { success: true, message: 'issue deleted successfully' };
+//     } catch (error) {
+//       throw new Error(`Failed to delete issues: ${error.message}`);
+//     }
+//   }
+// }
